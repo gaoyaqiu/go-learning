617eb7c5b3a99afabddb45a9ac22cb7fc81b7a25
gaoyaqiu <gyq30494613@gmail.com>
2020-11-29 17:53:09 +0800

    共享内存并发机制

---
+++ src/ch17/share_memory/share_memory_test.go
@@ -0,0 +1,43 @@
+package share_memory
+
+import (
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestCounterThreadSafe(t *testing.T) {
+	var mut sync.Mutex
+	counter := 0
+	for i := 0; i < 5000; i++ {
+		go func() {
+			defer func() {
+				mut.Unlock()
+			}()
+			mut.Lock()
+			counter++
+		}()
+	}
+	// 不等待的话，怕协程还没执行完就退出，导致数据不准确
+	time.Sleep(time.Second * 1)
+	t.Logf("counter = %d", counter)
+}
+
+func TestCounterWaitGroup(t *testing.T) {
+	var mut sync.Mutex
+	var wg sync.WaitGroup
+	counter := 0
+	for i := 0; i < 5000; i++ {
+		wg.Add(1)
+		go func() {
+			defer func() {
+				mut.Unlock()
+			}()
+			mut.Lock()
+			counter++
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	t.Logf("counter = %d", counter)
+}
