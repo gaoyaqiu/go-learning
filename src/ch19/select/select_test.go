4c412c592be8a8f543130fc75b24d343a0020e59
gaoyaqiu <gyq30494613@gmail.com>
2020-11-29 18:37:20 +0800

    多路选择和超时

---
+++ src/ch19/select/select_test.go
@@ -0,0 +1,38 @@
+package my_select
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func service() string {
+	time.Sleep(time.Microsecond * 50)
+	return "Done"
+}
+
+func otherTask() {
+	fmt.Println("working on something else")
+	time.Sleep(time.Microsecond * 100)
+	fmt.Println("Task is done.")
+}
+func AsyncService() chan string {
+	//retCh := make(chan string)
+	retCh := make(chan string, 1) // 更高效的做法
+	go func() {
+		ret := service()
+		fmt.Println("returned result.")
+		retCh <- ret
+		fmt.Println("service exited.")
+	}()
+	return retCh
+}
+
+func TestSelect(t *testing.T) {
+	select {
+	case ret := <-AsyncService():
+		t.Log(ret)
+	case <-time.After(time.Microsecond * 100):
+		t.Error("time out")
+	}
+}
